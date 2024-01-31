6f2b50d97000bd013474d3728efdf8bd0934da57
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-31 18:34:04 +0530

    add: tables

--- src/TableColumns/index.tsx
+++ src/TableColumns/index.tsx
@@ -1,2 +1,3 @@
 export { default as UserTableColumn } from "./UserTableColumns";
 export { default as ProductTableColumn } from "./ProductTableColumns";
+export { default as CartColumn } from "./CartColumns";
