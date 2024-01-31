ab268308df3df092a79e5d76cd74afe9d7089a6f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-01-31 18:32:52 +0530

    add: per page

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,11 +1,16 @@
 export const LOGIN_TITLE = "Login Your Account";
+
 export const LOGIN_DESCRIPTION =
     "Hey there! We are glad to see you again, Your journey continues here. Please take a moment to log in and dive into the healthy food and offers awaiting you.";
+
 export const SIGN_UP_TITLE = "Sign Up Your Account";
+
 export const SIGN_UP_DESCRIPTION =
     "To keep connected with us please signup with your personal information by email address and password.";
 
 export const OTP_VERIFY_TITLE = "Otp Verification";
+
+export const PER_PAGE = 6;
 
 export const getOtpDescription = (email: string) => {
     return (
