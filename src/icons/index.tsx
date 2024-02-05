805cdcce328444788e0ee3966d02c2ba3b2aff5e
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-05 23:44:09 +0530

    add: icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,2 +1,11 @@
 export { MdOutlineDriveFileRenameOutline as NameIcon } from "react-icons/md";
 export { MdOutlineDescription as DescriptionICon } from "react-icons/md";
+
+export { default as TotalOrdersIcon } from "./TotalOrdersIcon";
+export { default as UpArrowIcon } from "./UpArrowIcon";
+export { default as DownArrowIcon } from "./DownArrowIcon";
+export { default as CanceledOrderIcon } from "./CanceledOrderIcon";
+export { default as RevenueIcon } from "./RevenueIcon";
+export { default as DeliveredIcon } from "./DeliveredIcon";
+export { default as FilterIcon } from "./FilterIcon";
+export { default as ChefIcon } from "./ChefIcon";
